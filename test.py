5e7391f6ec16f2f708d9f7d7af4eb4c40f15ee5f
hunglongtrangithub <hunglongtran2004@gmail.com>
2024-01-02 20:54:57 -0500

    update: add some testing functions

--- test.py
+++ test.py
@@ -1,6 +1,9 @@
+from datetime import datetime
 from get_llm_responses import collect_llm_responses
-from get_evaluation_results import get_evaluation_results
+from get_evaluation_results import get_evaluation_results, normalize_string
+from get_answer_keys import get_answer_keys
 from main import load_questions, SYSTEM_PROMPT, make_user_prompt
+from helper import translate_principal_date
 import json
 
 
@@ -26,12 +29,12 @@ def test_get_evaluation_results():
     answer_keys = {
         "question1": "The capital of France is Paris.",
         "question2": "Python is an interpreted, high-level, general-purpose programming language.",
-        "question3": "The square root of 9 is 3.",
+        "question3": "medical document's principal date: January 23, 2010 at 10:45 AM",
     }
     responses = {
         "question1": "Paris is the capital of France.",
         "question2": "Python is a high-level, interpreted, general-purpose programming language.",
-        "question3": "3 is the square root of 9.",
+        "question3": "The principal date of the medical document is January 23, 2010 at 10:45.",
     }
     # Evaluate the responses
     evaluations = get_evaluation_results(responses, answer_keys)
@@ -44,7 +47,30 @@ def test_make_user_prompt():
     print(make_user_prompt(question, document))
 
 
+def test_get_answer_keys():
+    patient_name = "fake_patient1"
+    document_name = "fake_patient1_doc1_RAD"
+    answer_keys = get_answer_keys(patient_name, document_name)
+    print(json.dumps(answer_keys, indent=4))
+
+
+def test_translate_principal_date():
+    # Example usage
+    principal_date = "201001231045"
+    print(translate_principal_date(principal_date))
+
+
+def test_normalize_string():
+    # Example usage
+    s = "The principal date of the medical document is January 23, 2010 at 10:45 AM."
+    print(normalize_string(s))
+
+
 if __name__ == "__main__":
     # test_collect_llm_responses()
     # test_get_evaluation_results()
-    test_make_user_prompt()
+    # test_make_user_prompt()
+    # test_get_answer_keys()
+    # test_translate_principal_date()
+    test_normalize_string()
+    pass
